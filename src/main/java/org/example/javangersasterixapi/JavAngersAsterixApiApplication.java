a095ae129e9b3b26e23915ba15340c5907410d25
Eric Schmidt <eric0802@web.de>
2025-04-16 14:36:37 +0200

    Initial commit

---
+++ src/main/java/org/example/javangersasterixapi/JavAngersAsterixApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.javangersasterixapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavAngersAsterixApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavAngersAsterixApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javangersasterixapi/JavAngersAsterixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javangersasterixapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavAngersAsterixApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
