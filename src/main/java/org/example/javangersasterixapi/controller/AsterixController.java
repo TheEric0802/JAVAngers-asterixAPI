2a13237839868a53a9155c6b1a69b54692a72e98
Eric Schmidt <eric0802@web.de>
2025-04-16 20:15:56 +0200

    add methods getCharacterById, addCharacter, updateCharacter, deleteCharacter

--- src/main/java/org/example/javangersasterixapi/controller/AsterixController.java
+++ src/main/java/org/example/javangersasterixapi/controller/AsterixController.java
@@ -3,9 +3,7 @@ package org.example.javangersasterixapi.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.javangersasterixapi.model.AsterixCharacter;
 import org.example.javangersasterixapi.repository.AsterixCharacterRepo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,25 @@ public class AsterixController {
     @GetMapping
     public List<AsterixCharacter> getCharacters() {
         return repo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public AsterixCharacter getCharacterById(@PathVariable String id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public void addCharacter(@RequestBody AsterixCharacter character) {
+        repo.save(character);
+    }
+
+    @PutMapping
+    public void updateCharacter(@RequestBody AsterixCharacter character) {
+        repo.save(character);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCharacter(@PathVariable String id) {
+        repo.deleteById(id);
     }
 }
